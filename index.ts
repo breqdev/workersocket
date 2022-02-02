b57e0f0984e886b2440285e26c6bc66104dfad29
Brooke <breq@breq.dev>
2022-02-02 16:23:18 -0500

    Basic test suite

--- index.ts
+++ index.ts
@@ -25,17 +25,26 @@ const workerImpl = () => {
         socket.binaryType = data.binaryType;
       }
     } else if (data.type === "send") {
-      if (socket) {
+      if (socket && socket.readyState === WebSocket.OPEN) {
         socket.send(data.data);
       }
     } else if (data.type === "close") {
       if (socket) {
         socket.close();
       }
-      self.close();
+    } else {
+      console.warn("[workersocket] unknown message type", data.type);
     }
   });
 };
+
+const workerURL = URL.createObjectURL(
+  new Blob(["(" + workerImpl.toString() + ")();"], {
+    type: "text/javascript",
+  })
+);
+
+const worker = new Worker(workerURL);
 
 type WorkerSocketEvent = "open" | "close" | "error" | "message";
 
@@ -43,6 +52,7 @@ class WorkerSocket {
   worker: Worker | null;
 
   readyState = 0;
+
   listeners: {
     open: Array<() => void>;
     close: Array<() => void>;
@@ -55,17 +65,13 @@ class WorkerSocket {
     message: [],
   };
 
-  constructor(url: string, protocols: string[] = []) {
-    const workerURL = URL.createObjectURL(
-      new Blob(["(" + workerImpl.toString() + ")();"], {
-        type: "text/javascript",
-      })
-    );
+  workerMessageHandler: (event: any) => void;
 
-    this.worker = new Worker(workerURL);
+  constructor(url: string, protocols: string[] = []) {
+    this.worker = worker;
     this.worker.postMessage({ type: "create", url, protocols });
 
-    this.worker.onmessage = ({ data }) => {
+    this.workerMessageHandler = ({ data }: any) => {
       if (data.type === "open") {
         this.readyState = 1;
         this.listeners.open.forEach((listener) => listener());
@@ -81,6 +87,8 @@ class WorkerSocket {
         );
       }
     };
+
+    this.worker.addEventListener("message", this.workerMessageHandler);
   }
 
   set binaryType(binaryType: "blob" | "arraybuffer") {
@@ -93,6 +101,12 @@ class WorkerSocket {
 
   close() {
     this.worker?.postMessage({ type: "close" });
+
+    this.worker?.removeEventListener("message", this.workerMessageHandler);
+
+    this.listeners.close.forEach((listener) => listener());
+
+    this.worker = null;
   }
 
   addEventListener(
@@ -117,19 +131,19 @@ class WorkerSocket {
     ) as Array<() => void>;
   }
 
-  set onopen(listener: () => void) {
+  set onopen(listener: () => void | null) {
     this.listeners.open = listener ? [listener] : [];
   }
 
-  set onclose(listener: () => void) {
+  set onclose(listener: () => void | null) {
     this.listeners.close = listener ? [listener] : [];
   }
 
-  set onerror(listener: (error: Event) => void) {
+  set onerror(listener: (error: Event) => void | null) {
     this.listeners.error = listener ? [listener] : [];
   }
 
-  set onmessage(listener: (message: MessageEvent) => void) {
+  set onmessage(listener: (message: MessageEvent) => void | null) {
     this.listeners.message = listener ? [listener] : [];
   }
 }
